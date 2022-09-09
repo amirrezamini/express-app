99c2344822502498db9738dc85e2bfeb78cdaea3
Amirreza <a.amini8321@gmail.com>
2022-09-10 01:43:13 +0430

    Add_Pug-layout

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
 require('dotenv').config()
 
+
 const express = require('express')
 const path = require('path')
 const bodyParser = require('body-parser')
 
-const defaultRouter = require('./src/routes/index')
+
+const defaultRouter = require('./src/routes')
+const admin = require('./src/routes/admin')
+
 
 const app = express();
 const PORT = process.env.PORT
 
-app.use(bodyParser.urlencoded({extended: false}))
+
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'src', 'public', 'css')))
+
 app.use(defaultRouter);
+app.use('/admin', admin.routes);
+
+app.use((req, res, next) => {
+    res.status(404).render('error', { pageTitle: 'Page Not Found' })
+})
+
 
 app.set('view engine', 'pug')
 app.set('views', 'src/views')
+
 
 app.listen(PORT)
