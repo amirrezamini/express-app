fbe1dd63767e130c6031162836464220a46b85a3
Amirreza <a.amini8321@gmail.com>
2022-09-12 05:58:07 +0430

    ADD_models-feature

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,14 +1,16 @@
-const products = []
+const Product = require('../models/product')
 
 exports.getAddProduct = (req, res, next) => {
     res.render('admin/add-product')
 }
 
 exports.postAddProduct = (req, res, next) => {
-    products.push({ title: req.body.title })
+    const product = new Product(req.body.title)
+    product.save()
     res.redirect('/')
 }
 
 exports.getProducts = (req, res, next) => {
-    res.render('Shop', {products})
+    const products = Product.fetchAll()
+    res.render('Shop', { products })
 }
